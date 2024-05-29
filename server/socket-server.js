a83bb6af2acbb80333b11aa57eec5a704875ab58
Paul Wagner <hydrofin@gmail.com>
2024-05-29 17:06:13 +0200

    preserve rooms

--- server/socket-server.js
+++ server/socket-server.js
@@ -27,25 +27,38 @@ export function injectSocket(server) {
             id: Date.now(),
             name: name(),
             message: "",
+            room: room,
         }
         users[user.id] = user
 
         // notify about user join
-        socket.emit("self", { userId: user.id, user: user, users: users })
-        io.emit("join", { userId: user.id, user: user })
+        socket.emit("self", {
+            userId: user.id,
+            user: user,
+            users: Object.fromEntries(Object.entries(users).filter(([_, u]) => u.room == room))
+        })
+        io.to(room).emit("join", { userId: user.id, user: user })
 
         // forward user messages with their id
         socket.on('update', (update) => {
             console.log('user update', user.id, update);
-            users[user.id] = {...users[user.id], ...update}
-            io.emit("update", { userId: user.id, user: users[user.id] })
+            users[user.id] = {
+                // user base
+                ...users[user.id],
+                // update fields
+                ...update,
+                // preserver 'id' and 'room'
+                id: user.id,
+                room: user.room,
+            }
+            io.to(room).emit("update", { userId: user.id, user: users[user.id] })
         });
 
         // notify about user leave
         socket.on('disconnect', () => {
             console.log('user disconnect', user.id);
             delete users[user.id]
-            io.emit("leave", { userId: user.id })
+            io.to(room).emit("leave", { userId: user.id })
         });
     });
 }
